8f505e115974b9976128b10bd5a64ebbc8a3aef1
AtomicScience <dmitry.greblyuk@mail.ru>
2021-11-22 18:02:55 +0300

    <iframe> rendering complete

--- src/ui.ts
+++ src/ui.ts
@@ -1,14 +1,2 @@
 /// <reference lib="dom" />
 import './ui.css'
-
-document.onload = () => {
-  document.getElementById('create').onclick = () => {
-    const textbox = (<HTMLInputElement>document.getElementById('count'));
-    const count = parseInt(textbox.value, 10);
-    parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*')
-  }
-
-  document.getElementById('cancel').onclick = () => {
-    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
-  }
-}
