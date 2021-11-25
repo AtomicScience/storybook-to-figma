f82ca61ed4113c45d84ebda7e40a2c93a5fb00e0
AtomicScience <dmitry.greblyuk@mail.ru>
2021-11-25 17:01:27 +0300

    Figma plugin webkit added

--- src/ui.ts
+++ src/ui.ts
@@ -1,4 +1,10 @@
 /// <reference lib="dom" />
 import './ui.css'
+import { selectMenu, disclosure } from 'figma-plugin-ds';
 
 window.addEventListener('message', (e) => window.parent.postMessage(e.data, '*'));
+
+window.onload = () => {
+    selectMenu.init()
+    disclosure.init()
+}
