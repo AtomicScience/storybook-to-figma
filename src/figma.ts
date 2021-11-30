425bd3f1c500e183cf6464d8a8e6d840e217afe8
AtomicScience <dmitry.greblyuk@mail.ru>
2021-11-30 12:47:37 +0300

    Demo almost done, slight troubleshoot needed

--- src/figma.ts
+++ src/figma.ts
@@ -6,6 +6,17 @@ figma.showUI(__html__, {
 })
 
 figma.ui.onmessage = async (message) => {
+  console.log(message);
+  let data = message.data;
+
+  if(data) {
+    await processAddonMessage(message)
+  } else {
+    await processPluginMessage(message)
+  }
+}
+
+async function processAddonMessage(message : any) : Promise<void> {
   await figma.loadFontAsync(defaultFont);
 
   let baseFrame: PageNode | FrameNode = figma.currentPage;
@@ -15,11 +26,36 @@ figma.ui.onmessage = async (message) => {
 
   for (const { id, layer, position, componentData } of nodes) {
     if (position) {
-        const { x, y } = getDropOffset(position);
-        layer.x = x;
-        layer.y = y;
+      const { x, y } = getDropOffset(position);
+      layer.x = x;
+      layer.y = y;
     }
-    
+
     await addLayersToFrame([layer], baseFrame);
+  }
 }
+
+async function processPluginMessage(message : any) {
+  if(message?.request == "READ_TODO_STORAGE") {
+    console.log("READ_TODO_STORAGE");
+    let todoEntries = await figma.clientStorage.getAsync("todo_entries");
+    
+    let responce = {
+      result: todoEntries || [],
+      timestamp: message.timestamp
+    }
+
+    figma.ui.postMessage(responce);
+  } else if(message?.request == "WRITE_TODO_STORAGE") {
+    console.log("WRITE_TODO_STORAGE");
+    let todoEntries = message.payload;
+
+    let responce = {
+      result: "OK",
+      timestamp: message.timestamp
+    }
+
+    await figma.clientStorage.setAsync("todo_entries", todoEntries);
+    figma.ui.postMessage(responce);
+  }
 }
--- src/modules/todoList.ts
+++ src/modules/todoList.ts
@@ -1,25 +1,58 @@
 /// <reference lib="dom" />
-function init(): void {
-    let items = ["First", "Second", "Third"];
-
-    items.forEach(addItemToThePage);
-
-    document
-        .querySelectorAll(".todo_item_actual")
-        .forEach((element) => element.addEventListener("click", onActualTodoItemClick));
+interface Request {
+    request: string,
+    payload?: any,
+    timestamp: number
 }
 
-function onActualTodoItemClick(e : MouseEvent) : void {
+let items = [];
+
+function init(): void {
+    let itemsPromise = getItemsFromFigmaStorage();
+
+    itemsPromise.then((resolvedItems) => {
+        items = resolvedItems;
+
+        updateTodoList();
+
+        document
+            .getElementById("todo_button")
+            .addEventListener("click", onTodoAddButtonClick);
+    })
+}
+
+async function onActualTodoItemClick(e: MouseEvent) : Promise<void> {
     let clickedElement = e.target as HTMLElement;
 
     clickedElement.classList.remove("todo_item_actual");
     clickedElement.classList.add("todo_item_done");
 
-    scheduleRemoval(clickedElement, 300);
+    setTimeout(() => removeItem(clickedElement.innerText), 300);
 }
 
-function scheduleRemoval(element : HTMLElement, timeout : number) : void {
-    setTimeout(() => element.remove(), timeout);
+async function onTodoAddButtonClick(e : MouseEvent) : Promise<void> {
+    let input = document.getElementById("todo_input") as HTMLInputElement;
+
+    items.push(input.value);
+    updateTodoList();
+
+    await saveItemsToFigmaStorage();
+}
+
+async function removeItem(contents : string) : Promise<void> {
+    items = items.filter(item => item !== contents)
+
+    updateTodoList();
+    await saveItemsToFigmaStorage();
+}
+
+function updateTodoList() : void {
+    document.getElementById("todo_list").innerHTML = '';
+    items.forEach(addItemToThePage);
+
+    document
+        .querySelectorAll(".todo_item_actual")
+        .forEach((element) => element.addEventListener("click", onActualTodoItemClick));
 }
 
 function addItemToThePage(item : string) : void {
@@ -36,6 +69,43 @@ function constructItemElement(item: string) {
     element.innerText = item;
 
     return element;
+}
+
+async function getItemsFromFigmaStorage() : Promise<string[]> {
+    let request = createAndSendRequestToFigma("READ_TODO_STORAGE");
+
+    return await createPromiseForRequestResult(request);
+}
+
+async function saveItemsToFigmaStorage() : Promise<string> {
+    let request = createAndSendRequestToFigma("WRITE_TODO_STORAGE", items);
+
+    return await createPromiseForRequestResult(request);
+}
+
+function createAndSendRequestToFigma(request : string, payload? : any) : Request {
+    let requestMessage = {
+        request: request,
+        timestamp: Date.now() // Timestamp serves as an ID for requests and responces
+    };
+
+    window.parent.postMessage({ pluginMessage: requestMessage} , "https://www.figma.com");
+
+    return requestMessage;
+}
+
+function createPromiseForRequestResult(request : Request) : Promise<any> {
+    return new Promise((resolve, reject) => {
+        window.addEventListener('message', (e) => {
+            let responce = e.data.pluginMessage;
+
+            if(responce?.timestamp === request.timestamp) {
+                resolve(responce.result);
+            }
+        });
+
+        setTimeout(() => reject("Request timeout"), 500);
+    })
 }
 
 export const todoList = {
