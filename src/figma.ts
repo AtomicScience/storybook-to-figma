5162c4fba20f76cc9f89d201702d013d10d40527
AtomicScience <dmitry.greblyuk@mail.ru>
2021-12-16 10:45:59 +0300

    Pulled FigmaStorage-related files from experimental branch

--- src/figma.ts
+++ src/figma.ts
@@ -1,4 +1,7 @@
 import { defaultFont, getDropOffset, addLayersToFrame } from 'html-figma/figma';
+import FigmaRequestListener from './modules/figmaStorage/FigmaRequestListener';
+
+const figmaRequestListener = new FigmaRequestListener(figma);
 
 figma.showUI(__html__, {
   width: 750,
@@ -6,6 +9,17 @@ figma.showUI(__html__, {
 })
 
 figma.ui.onmessage = async (message) => {
+  console.log(message);
+  let data = message.data;
+
+  if(data) {
+    await processAddonMessage(message)
+  } else {
+    await figmaRequestListener.processPossibleRequest(message);
+  }
+}
+
+async function processAddonMessage(message : any) : Promise<void> {
   await figma.loadFontAsync(defaultFont);
 
   let baseFrame: PageNode | FrameNode = figma.currentPage;
@@ -15,11 +29,11 @@ figma.ui.onmessage = async (message) => {
 
   for (const { id, layer, position, componentData } of nodes) {
     if (position) {
-        const { x, y } = getDropOffset(position);
-        layer.x = x;
-        layer.y = y;
+      const { x, y } = getDropOffset(position);
+      layer.x = x;
+      layer.y = y;
     }
-    
+
     await addLayersToFrame([layer], baseFrame);
-}
+  }
 }
