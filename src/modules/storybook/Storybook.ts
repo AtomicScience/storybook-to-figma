8dac86c853d7ec53ada06762df3cf557dae9735c
AtomicScience <dmitry.greblyuk@mail.ru>
2021-12-20 16:53:01 +0300

    Storybook objects now contain UUIDs

--- src/modules/storybook/Storybook.ts
+++ src/modules/storybook/Storybook.ts
@@ -1,4 +1,13 @@
-export default interface Storybook {
+import { v4 as uuidv4 } from 'uuid';
+
+export default class Storybook {
     name : string;
     address : string;
+    id: string;
+
+    constructor(name : string, address : string, id? : string) {
+        this.name = name;
+        this.address = address;
+        this.id = id || uuidv4();
+    }
 }
