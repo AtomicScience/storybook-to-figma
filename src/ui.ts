88721e7f1f3bce9107024f74a76c168323bf6f5f
AtomicScience <dmitry.greblyuk@mail.ru>
2021-12-20 19:06:40 +0300

    Iframe address changing logic added

--- src/ui.ts
+++ src/ui.ts
@@ -5,6 +5,7 @@ import ModalWindow from './modules/ui/ModalWindow'
 import StorybookAdder from './modules/ui/StorybookAdder';
 import StorybookStorage from './modules/storybook/StorybookStorage';
 import StorybookChooser from './modules/ui/StorybookChooser';
+import Iframe from './modules/ui/Iframe';
 
 window.addEventListener('message', (e) => window.parent.postMessage(e.data, '*'));
 
@@ -16,4 +17,6 @@ window.onload = () => {
 
     new StorybookAdder(storage).init();
     new StorybookChooser(storage).init();
+
+    new Iframe(storage).init();
 }
